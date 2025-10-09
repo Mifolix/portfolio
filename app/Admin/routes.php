ae527c35b7a5e3d0b2e7a18741aad0ef61186db9
Mifolix <mifolix@mail.ru>
2025-10-09 10:28:41 +0300

    add route for technologies

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Admin\Controllers\ExperienceController;
+use App\Admin\Controllers\TechnologyController;
 use App\Admin\Controllers\UserController;
 use Illuminate\Routing\Router;
 
@@ -16,5 +17,6 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('home');
     $router->resource('/experiences', ExperienceController::class);
     $router->resource('/users', UserController::class);
+    $router->resource('/technologies', TechnologyController::class);
 
 });
