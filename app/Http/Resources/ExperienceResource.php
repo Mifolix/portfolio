b6887a90660a378d5ae50a3b4406d12478d63298
Mifolix <mifolix@mail.ru>
2025-10-17 19:48:32 +0300

    add resource for experiences

---
+++ app/Http/Resources/ExperienceResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
+use OpenApi\Attributes\Xml;
+
+#[Schema(
+    title: 'ExperienceResource',
+    description: 'Experience',
+    properties: [
+        new Property(
+            property: 'id',
+            description: 'Идентификатор записи',
+            type: 'integer',
+        ),
+        new Property(
+            property: 'position',
+            description: 'Должность',
+            type: 'string',
+        ),
+        new Property(
+            property: 'short_description',
+            description: 'Описание',
+            type: 'string',
+        ),
+        new Property(
+            property: 'date_from',
+            description: 'Дата от',
+            type: 'string',
+            format: 'date',
+        ),
+        new Property(
+            property: 'date_to',
+            description: 'Дата до',
+            type: 'string',
+            format: 'date',
+        ),
+        new Property(
+            property: 'company',
+            description: 'Компания',
+            type: 'string',
+        ),
+        new Property(
+            property: 'technologies',
+            description: 'Технологии',
+            type: 'string',
+        ),
+    ],
+    xml: new Xml(
+        name: 'ExperienceResource'
+    )
+)]
+class ExperienceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'position' => $this->position,
+            'short_description' => $this->short_description,
+            'date_from' => Carbon::parse($this->date_from)->translatedFormat('F Y'),
+            'date_to' => Carbon::parse($this->date_to)->translatedFormat('F Y'),
+            'company' => $this->company,
+            'technologies' => TechnologyResource::collection($this->technologies),
+        ];
+    }
+}
